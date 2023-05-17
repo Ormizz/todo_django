11f8a5a71f3b33f38cbcba2206060e42cc97678e
Ormizz <urielsoro@gmail.com>
2023-05-17 12:21:28 +0200

    mess

--- todo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from rest_framework import routers
-
-from todo.views import TodoViewSet, TodoListViewSet
-
-router = routers.DefaultRouter()
-router.register('todos', TodoViewSet)
-router.register('todo-lists', TodoListViewSet)
--- user_management/urls.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import routers
-
-from user_management.views import MeViewSet
-
-router = routers.DefaultRouter()
-router.register('me', MeViewSet, basename='me')
